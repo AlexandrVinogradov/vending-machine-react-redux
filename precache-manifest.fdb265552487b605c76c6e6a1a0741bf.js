d9c34462fa1e56431b8f2e97fae4d2867f3c26d3
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-01-17 19:24:13 +0300

    Updates

---
+++ precache-manifest.fdb265552487b605c76c6e6a1a0741bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442970d1c3a1a7806c2d5bf99a33b112",
+    "url": "/vending-machine-react/index.html"
+  },
+  {
+    "revision": "6a09676fcdf583ac2417",
+    "url": "/vending-machine-react/static/css/main.8d709045.chunk.css"
+  },
+  {
+    "revision": "6b1e8d98bb49fd377c54",
+    "url": "/vending-machine-react/static/js/2.aae89e8c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vending-machine-react/static/js/2.aae89e8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a09676fcdf583ac2417",
+    "url": "/vending-machine-react/static/js/main.5c486484.chunk.js"
+  },
+  {
+    "revision": "39769b6a3d4c32ac285a",
+    "url": "/vending-machine-react/static/js/runtime-main.949caf58.js"
+  }
+]);
