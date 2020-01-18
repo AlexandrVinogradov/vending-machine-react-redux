d3d356bce5632809686664f70e6c88d7b31baa1e
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-01-18 22:43:14 +0300

    change trouble

--- src/App.js
+++ src/App.js
@@ -5,38 +5,139 @@ import './App.css';
 class App extends React.Component {
 
   state = {
+    change: 0,
     balance: 0,
-    inputValue: 0,
+    selectedProduct: 0,
+
+    productInputValue: 0,
+    balanceInputValue: 0,
+
     legalPayload: [50, 100, 200, 500, 1000],
-    errorMessage: '',
+    // correctProductId: [1, 2, 3, 4, 5, 6],
+    errorMessageUnknowBancnote: '',
+    errorMessageIncorrect: '',
     isError: false,
     products: [
-      { name: 'Oreo', price: 80, desc: 'Cookie', id: '1' },
-      { name: 'Milka', price: 140, desc: 'Chocolate', id: '2' },
-      { name: 'M&M', price: 200, desc: 'Candy', id: '3' },
-      { name: 'Twix', price: 90, desc: 'Choco bar', id: '4' },
-      { name: 'Mentos', price: 50, desc: 'Chewing pills', id: '5' },
-      { name: 'Mountain dew', price: 170, desc: 'Cold drink', id: '6' },
-    ]
+      { name: 'Oreo', price: 80, desc: 'Cookie', id: 1 },
+      { name: 'Milka', price: 140, desc: 'Chocolate', id: 2 },
+      { name: 'M&M', price: 200, desc: 'Candy', id: 3 },
+      { name: 'Twix', price: 90, desc: 'Choco bar', id: 4 },
+      { name: 'Mentos', price: 50, desc: 'Chewing pills', id: 5 },
+      { name: 'Mountain dew', price: 170, desc: 'Cold drink', id: 6 },
+    ],
   }
 
   render() {
-    const handleEnterClick = (e) => {
+    let money = 57
+    let bills = [10, 5, 2, 1]
+    let p = 0
+     while (p === money) {
+      let i = 0
+      let z = Math.floor((money-p)/bills[i]) 
+      p += bills[i] * z 
+      i++
+     } 
+     console.log(p);
+
+    //======================================================================
+    // var bills = [5, 10, 20, 50, 100];
+    // var money = mod(89);
+    
+    // function mod(num){
+    //     if (num % 5 === 0){
+    //         return num;
+    //     }else{
+    //         return num + 5 - num % 5
+    //     }
+    // }
+    
+    // function foo(num){
+    //     var index = bills.length - 1;
+    //     var splits = [];
+    //     while (money >= bills[0]){
+    //         if (money >= bills[index]){
+    //            money -= bills[index];
+    //            splits.push(bills[index]);
+    //         }else{
+    //             index--;
+    //         }
+    //     }
+    //     return splits;
+    // }
+    // console.log(foo(money));
+
+    //====================================================================
+    // const makeChange = (change) => {
+    //   if (change < 0)
+    //     return Number.MAX_VALUE;
+    
+    //   if (change === 1 || change === 2 || change === 5 || change === 10) {
+    //     return 1;
+    //   }
+    
+    //   let min1 = makeChange(change - 1);
+    //   let min2 = makeChange(change - 2);
+    //   let min5 = makeChange(change - 5);
+    //   let min10 = makeChange(change - 10);
+    
+    //   return Math.min(min1, min2, min5, min10) + 1;
+    // }
+    //console.log(makeChange(100));
+
+    const foundSelectedProduct = this.state.products.find(p => p.id === this.state.selectedProduct)
+    const foundInputSelectedProduct = this.state.products.find(p => p.id === this.state.productInputValue)
+
+    const handleBalanceEnterClick = (e) => {
       e.preventDefault();
-      if (this.state.legalPayload.includes(parseInt(this.state.inputValue))) {
+      if (this.state.legalPayload.includes(parseInt(this.state.balanceInputValue))) {
         this.setState({
-          balance: this.state.inputValue
+          balance: parseInt(this.state.balanceInputValue) + parseInt(this.state.balance),
+          errorMessageUnknowBancnote: '',
         })
       } else {
         this.setState({
           isError: true,
-          errorMessage: 'Unknow bancnote'
+          errorMessageUnknowBancnote: 'Unknow bancnote'
         })
       }
     }
-    const inputValue = (e) => {
+    const balanceInputValue = (e) => {
       this.setState({
-        inputValue: e.currentTarget.value
+        balanceInputValue: e.currentTarget.value
+      })
+    }
+
+    // console.log(this.state.selectedProduct ? foundInputSelectedProduct.price : null);
+    // console.log(this.state.change);
+
+    const handleProductEnterClick = (e) => {
+      e.preventDefault();
+      if (this.state.products.some(p => p.id === this.state.productInputValue) && this.state.balance >= foundInputSelectedProduct.price) {
+        this.setState({
+          selectedProduct: parseInt(this.state.productInputValue),
+          errorMessageIncorrect: '',
+          change: parseInt(this.state.balance) - foundInputSelectedProduct.price
+        })
+      } else if (this.state.products.some(p => p.id === this.state.productInputValue) && this.state.balance < foundInputSelectedProduct.price) {
+        this.setState({
+          isError: true,
+          errorMessageIncorrect: 'Not enought money'
+        })
+      } else {
+        this.setState({
+          isError: true,
+          errorMessageIncorrect: 'Enter correct number'
+        })
+      }
+    }
+    const productInputValue = (e) => {
+      this.setState({
+        productInputValue: parseInt(e.currentTarget.value)
+      })
+    }
+    const takeProduct = () => {
+      this.setState({
+        balance: 0
       })
     }
 
@@ -62,24 +163,29 @@ class App extends React.Component {
       </div>
 
       <div className='interface__control-panel'>
-        <form onSubmit={handleEnterClick} className='form'>
+        <form onSubmit={handleBalanceEnterClick} className='form'>
           <div className='dialog-board'>
-            <span>Insert banknotes...</span>
-            <span>{this.state.balance}</span>
+            <span>{this.state.errorMessageUnknowBancnote ? this.state.errorMessageUnknowBancnote : this.state.balance === 0 ? 'Insert banknotes...' : `Inserted money: ${this.state.balance}`}</span>
           </div>
 
-          <input onInput={inputValue} />
+          <input onInput={balanceInputValue} disabled={this.state.balance > 499 || this.state.selectedProduct} />
 
           <p className='form__desc'>Available banknotes: 50, 100,
             200, 500 or 1000 R.
             The machine gives change
             in 1, 2, 5 and 10 R coins.</p>
         </form>
-        <form>
+        <form onSubmit={handleProductEnterClick}>
           <div className='dialog-board'>
-            <span>{this.state.balance === 0 ? '>' : 'Choose product'}</span>
+            <span>{this.state.errorMessageIncorrect ? this.state.errorMessageIncorrect :
+             this.state.balance === 0 ? '>' : 
+             this.state.selectedProduct ? 'Success' : 'Choose product'}</span>
           </div>
-          <input />
+
+
+          <input onInput={productInputValue} disabled={this.state.selectedProduct || this.state.balance === 0} />
+
+
         </form>
         <div>
           <div className='dialog-board'>
@@ -89,11 +195,11 @@ class App extends React.Component {
             <div className='product'>
               <span>10R <br /> 30 Coins</span>
             </div>
-            <div className='product'>
-              <span>name</span>
-              <span>desc</span>
-              <span>price</span>
-              <span>id</span>
+            <div className='product' onClick={takeProduct}>
+              <span>{this.state.selectedProduct ? foundSelectedProduct.name : null}</span>
+              <span>{this.state.selectedProduct ? foundSelectedProduct.desc : null}</span>
+              <span>{this.state.selectedProduct ? foundSelectedProduct.price : null}</span>
+              <span>{this.state.selectedProduct ? foundSelectedProduct.id : null}</span>
             </div>
           </div>
         </div>
