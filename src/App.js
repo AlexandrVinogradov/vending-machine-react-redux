e4834214b463ab4d7e2170c63edd6f40333afa9a
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-01-18 13:07:20 +0300

    render of object

--- src/App.js
+++ src/App.js
@@ -6,87 +6,69 @@ class App extends React.Component {
 
   state = {
     balance: 0,
-
+    inputValue: 0,
+    legalPayload: [50, 100, 200, 500, 1000],
+    errorMessage: '',
+    isError: false,
     products: [
-      {name: 'Oreo',   price: '80',  desc: 'Cookie', id: '1'},
-      {name: 'Milka',  price: '140', desc: 'Chocolate', id: '2'},
-      {name: 'M&M',    price: '200', desc: 'Candy', id: '3'},
-      {name: 'Twix',   price: '90',  desc: 'Choco bar', id: '4'},
-      {name: 'Mentos', price: '50',  desc: 'Chewing pills', id: '5'},
-      {name: 'Mountain dew', price: '170', desc: 'Cold drink', id: '6'},
+      { name: 'Oreo', price: 80, desc: 'Cookie', id: '1' },
+      { name: 'Milka', price: 140, desc: 'Chocolate', id: '2' },
+      { name: 'M&M', price: 200, desc: 'Candy', id: '3' },
+      { name: 'Twix', price: 90, desc: 'Choco bar', id: '4' },
+      { name: 'Mentos', price: 50, desc: 'Chewing pills', id: '5' },
+      { name: 'Mountain dew', price: 170, desc: 'Cold drink', id: '6' },
     ]
   }
 
   render() {
-
     const handleEnterClick = (e) => {
+      e.preventDefault();
+      if (this.state.legalPayload.includes(parseInt(this.state.inputValue))) {
+        this.setState({
+          balance: this.state.inputValue
+        })
+      } else {
+        this.setState({
+          isError: true,
+          errorMessage: 'Unknow bancnote'
+        })
+      }
+    }
+    const inputValue = (e) => {
       this.setState({
-        balance: e.currentTarget.value
+        inputValue: e.currentTarget.value
       })
     }
+
+    const NewProductElement = props => {
+      return <>
+        <li className='product-list__item'>
+            <div className='product'>
+              <span className='product__name'>{props.name}</span>
+              <span className='product__desc'>{props.desc}</span>
+              <span className='product__prise'>{props.price}</span>
+              <span className='product__name'>{props.id}</span>
+            </div>
+          </li>
+      </>
+    }
+    const newProductElement = this.state.products.map( p => <NewProductElement id={p.id} key={p.id} name={p.name} price={p.price} desc={p.desc} /> )
 
     return <div className='interface'>
       <div className='inteface__goods'>
         <ul className='product-list'>
-          <li className='product-list__item'>
-            <div className='product'>
-              <span className='product__name'>Oreo</span>
-              <span className='product__desc'>Cookie</span>
-              <span className='product__prise'>80 R</span>
-              <span className='product__name'>1</span>
-            </div>
-          </li>
-          <li className='product-list__item'>
-            <div className='product'>
-              <span className='product__name'>Milka</span>
-              <span className='product__desc'>Chocolate</span>
-              <span className='product__prise'>140 R</span>
-              <span className='product__name'>2</span>
-            </div>
-          </li>
-          <li className='product-list__item'>
-            <div className='product'>
-              <span className='product__name'>M&M</span>
-              <span className='product__desc'>Candy</span>
-              <span className='product__prise'>200 R</span>
-              <span className='product__name'>3</span>
-            </div>
-          </li>
-          <li className='product-list__item'>
-            <div className='product'>
-              <span className='product__name'>Twix</span>
-              <span className='product__desc'>Choco bar</span>
-              <span className='product__prise'>90 R</span>
-              <span className='product__name'>4</span>
-            </div>
-          </li>
-          <li className='product-list__item'>
-            <div className='product'>
-              <span className='product__name'>Mentos</span>
-              <span className='product__desc'>Chewing pills</span>
-              <span className='product__prise'>50 R</span>
-              <span className='product__name'>5</span>
-            </div>
-          </li>
-          <li className='product-list__item'>
-            <div className='product'>
-              <span className='product__name'>Mountain dew</span>
-              <span className='product__desc'>Cold drink</span>
-              <span className='product__prise'>170 R</span>
-              <span className='product__name'>6</span>
-            </div>
-          </li>
+        {newProductElement}
         </ul>
       </div>
-      
+
       <div className='interface__control-panel'>
-        <form className='form'>
+        <form onSubmit={handleEnterClick} className='form'>
           <div className='dialog-board'>
             <span>Insert banknotes...</span>
             <span>{this.state.balance}</span>
           </div>
 
-          <input  onKeyPress={handleEnterClick} />
+          <input onInput={inputValue} />
 
           <p className='form__desc'>Available banknotes: 50, 100,
             200, 500 or 1000 R.
@@ -105,7 +87,7 @@ class App extends React.Component {
           </div>
           <div className='conclusion'>
             <div className='product'>
-              <span>10R <br/> 30 Coins</span>
+              <span>10R <br /> 30 Coins</span>
             </div>
             <div className='product'>
               <span>name</span>
